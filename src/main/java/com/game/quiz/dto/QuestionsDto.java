af0645f6e97d043ea6b996614e18cfde7f4de52c
Alexander-Valenieks <aleksandrs.valenieks@gmail.com>
2023-08-02 20:32:08 +0300

    Bug fixes and improvements: - One game at a time; - Issues with displaying special characters; - Behavior for insufficient amount of questions from the API
    Added unit tests.

--- src/main/java/com/game/quiz/dto/QuestionsDto.java
+++ src/main/java/com/game/quiz/dto/QuestionsDto.java
@@ -3,8 +3,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor
 @Getter
@@ -12,7 +14,6 @@ public class QuestionsDto {
     @JsonProperty("response_code")
     private int responseCode;
     private List<QuestionDto> results;
-
 
     @NoArgsConstructor
     @Getter
@@ -27,5 +28,19 @@ public class QuestionsDto {
         @JsonProperty("incorrect_answers")
         private List<String> incorrectAnswers;
 
+        public void setQuestion(String question) {
+            this.question = HtmlUtils.htmlUnescape(question);
+        }
+
+        public void setCorrectAnswer(String correctAnswer) {
+            this.correctAnswer = HtmlUtils.htmlUnescape(correctAnswer);
+        }
+
+        public void setIncorrectAnswers(List<String> incorrectAnswers) {
+            List<String> newIncorrectAnswers = incorrectAnswers.stream()
+                    .map(answer -> HtmlUtils.htmlUnescape(answer))
+                    .collect(Collectors.toList());
+            this.incorrectAnswers = newIncorrectAnswers;
+        }
     }
 }
